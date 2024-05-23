a3d58ec9c0d407a0325ff8e0c3112a93aa15d561
Yunchuan "Winslow" Hu <i@winsloweric.com>
2024-05-23 07:22:08 -0700

    Fix detection of profile menu, bit reformat

--- foreground.js
+++ foreground.js
@@ -1,21 +1,5 @@
 const repUrl = "https://yuuta.moe/yuuta.jpg";
 
-function replaceImages() {
-    [...[...[...Array.from(document.querySelectorAll('img[alt]'))].filter(x => {
-        return x.alt.toLowerCase().indexOf("employee") != -1
-            && x.alt.toLowerCase().indexOf("photo") != -1
-    })], document.getElementsByClassName("wdappchrome-aax")[0]].forEach(y => {
-        if (y.src.indexOf("attachment") != -1) {
-            // Image covered by src attr
-            y.src = repUrl;
-        } else if (y.style["background-image"].indexOf("attachment") != -1) {
-            y.setAttribute("style", "background-image: url(" + repUrl + ");"); // Workday's dev is an idiot who writes style in element
-        } else {
-            console.log(y);
-        }
-    });
-}
-
 const observer = new MutationObserver((mutationsList, observer) => {
     for (let mutation of mutationsList) {
         if (mutation.type === 'childList' || mutation.type === 'attributes') {
@@ -30,4 +14,26 @@ observer.observe(document.body, {
     attributes: true
 });
 
-replaceImages();
+(replaceImages = () => {
+    [...[...[...Array.from(document.querySelectorAll('img[alt]'))].filter(x => {
+        return x.alt.toLowerCase().indexOf("employee") != -1
+            && x.alt.toLowerCase().indexOf("photo") != -1 ||
+            x?.parentElement?.parentElement?.parentElement?.tagName == "UL"
+    })], document.getElementsByClassName("wdappchrome-aax")[0]].forEach(y => {
+        // aax is dirty fix and will need to be replaced with more advanced parent-context checking
+        if (y != undefined) {
+            if (y?.src.indexOf("attachment") != -1) {
+                // Image covered by src attr
+                y.src = repUrl;
+            } else if (y?.style["background-image"].indexOf("attachment") != -1) {
+                // Workday's dev is an idiot who writes style in element
+                if (y.getAttribute("role") == "presentation") {
+                    y.src = repUrl;
+                    console.log(y);
+                } else {
+                    y.setAttribute("style", "background-image: url(" + repUrl + ");");
+                }
+            }
+        }
+    });
+})();
